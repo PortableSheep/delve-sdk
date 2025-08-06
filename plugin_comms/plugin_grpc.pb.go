45353717944e9aced9ca72366fa2fc644a56d381
Michael Gunderson <michael@portablesheep.com>
2025-08-05 21:30:44 -0500

    Generate

--- plugin_comms/plugin_grpc.pb.go
+++ plugin_comms/plugin_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v5.29.3
-// source: plugin_comms/plugin.proto
+// source: plugin.proto
 
 package plugin_comms
 
@@ -103,5 +103,5 @@ var PluginManager_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "plugin_comms/plugin.proto",
+	Metadata: "plugin.proto",
 }
